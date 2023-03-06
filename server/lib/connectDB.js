9e0fdc87756dc9a1d76d4d25df593c62fa384025
oyinkansola <adebayoalameen88@gmail.com>
2023-03-06 03:53:24 +0100

    Feat: Created the schema for the blog post

--- server/lib/connectDB.js
+++ server/lib/connectDB.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connect = () => {
-    console.log('MONGO_URI:', process.env.MONGO_URI);
   mongoose
     .connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
---
+++ server/model/blogSchema.js
@@ -0,0 +1,30 @@
+const { default: mongoose } = require("mongoose");
+
+const Schema = mongoose.Schema
+
+const Blogs = new Schema({
+    title: {
+        type: String,
+        required: true
+      },
+      body: {
+        type: String,
+        required: true
+      },
+      author: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      },
+      comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+      }]
+    });
+    
+    module.exports =  mongoose.model('blog', Blogs);
+    
+    
