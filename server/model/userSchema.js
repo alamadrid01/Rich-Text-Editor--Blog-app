1f9dc26cf9443ba2b9240f0b57cbc5b8684649af
alamadrid01 <adebayoalameen88@gmail.com>
2023-04-02 00:17:57 +0800

    Feat: Created the user schema

---
+++ server/model/userSchema.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const userProfileSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    website: {
+      type: String,
+      default: "",
+    },
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const UserProfile = mongoose.model("UserProfile", userProfileSchema);
+
+module.exports = UserProfile;
