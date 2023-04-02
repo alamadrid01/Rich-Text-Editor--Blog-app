88ed8d576cd5b9b10068dba15e55d7f06d0cd3a2
alamadrid01 <adebayoalameen88@gmail.com>
2023-04-03 07:52:38 +0800

    Feat: Created the bio edit modal

---
+++ client/src/components/Modal/bioModal.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const BioModal = ({ onClose }) => {
+  const [bio, setBio] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const handleClickOutside = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <>
+      <main
+        className=" z-10 fixed inset-0 bg-gray-900 bg-opacity-75 flex justify-center items-center "
+        onClick={handleClickOutside}
+      >
+        <div
+          className=" flex flex-col bg-white  rounded-md px-8 py-4 w-3/5 sm:w-4/5 lg:w-1/2"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="text-[20px] font-bold text-green-800 text-center">
+            Edit Bio
+          </div>
+          <form onSubmit={submitHandler} className="flex flex-col gap-3 mt-3">
+            <textarea
+              name="bio"
+              id="bio"
+              cols="25"
+              rows="9"
+              value={bio}
+              onChange={(e) => setBio(e.target.value)}
+              placeholder="Type your bio here ..."
+              className="focus focus:outline-none"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-green-400 text-white px-3 md:px-6 px-6 py-3 rounded-md hover:opacity-75 transition-all duration-300"
+            >
+              Save
+            </button>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default BioModal;
--- client/src/pages/Profile/profile.jsx
+++ client/src/pages/Profile/profile.jsx
@@ -5,109 +5,122 @@ import profile from "../../assets/profile.svg";
 import Edit from "../../assets/edited.svg";
 import Edited from "../../assets/edit.svg"
 import Logout from "../../assets/logout.svg"
+import BioModal from "../../components/Modal/bioModal";
+import { useState } from "react";
 
 const Profile = () => {
     const Navigate = useNavigate();
+    const [showBio, setShowBio] = useState(false);
+
+    const handleModalClose = () => {
+      setShowBio(false);
+    };
   return (
-    <section className="px-0 lg-px-5 xl:max-w-6xl xl:mx-auto pb-20">
-      <Navbar />
-      <main>
-        <div className="head flex justify-between items-center mt-5">
-          <h1 className="text-4xl text-[#262630]">Profile</h1>
-          <button
-            className="bg-black text-white px-3 md:px-6 px-6 py-3 rounded-md hover:opacity-75 transition-all duration-300"
-            onClick={() => Navigate("/create-blog")}
-          >
-            Create a blog
-          </button>
-        </div>
-        <div className="about flex flex-col gap-3 mt-5">
-          <section className="bg-white rounded-md mt-5 py-5 px-2">
-            <h2 className=" text-2xl text-[#262630]">My Account</h2>
-            <div className="flex gap-5 mt-10 px-4">
-              <div className="p-4 bg-gray-300 rounded-full">
-                <img src={profile} alt="" />
-              </div>
-              <div className="flex flex-col gap-3">
-                <div className="text-[20px] font-bold">Adebayo</div>
-                <div className="text-[15px]">Full Name</div>
-              </div>
-            </div>
-            <div className="flex gap-5 mt-6 px-4">
-              <div className="p-4 bg-gray-300 rounded-full">
-                <img src={Message} alt="" />
-              </div>
-              <div className="flex flex-col gap-3">
-                <div className="text-[20px] font-bold">
-                  adebayoalameen@gmail.com
+    <>
+      {showBio && <BioModal onClose={handleModalClose} />}
+      <section className="px-0 lg-px-5 xl:max-w-6xl xl:mx-auto pb-20">
+        <Navbar />
+        <main>
+          <div className="head flex justify-between items-center mt-5">
+            <h1 className="text-4xl text-[#262630]">Profile</h1>
+            <button
+              className="bg-black text-white px-3 md:px-6 px-6 py-3 rounded-md hover:opacity-75 transition-all duration-300"
+              onClick={() => Navigate("/create-blog")}
+            >
+              Create a blog
+            </button>
+          </div>
+          <div className="about flex flex-col gap-3 mt-5">
+            <section className="bg-white rounded-md mt-5 py-5 px-2">
+              <h2 className=" text-2xl text-[#262630]">My Account</h2>
+              <div className="flex gap-5 mt-10 px-4">
+                <div className="p-4 bg-gray-300 rounded-full">
+                  <img src={profile} alt="" />
                 </div>
-                <div className="text-[15px]">Email Address</div>
-              </div>
-            </div>
-            <div className="flex gap-5 mt-8 px-[40px] relative">
-              <div className="flex flex-col gap-3">
-                <div className="text-[20px] font-bold">Bio</div>
-                <div className="text-[15px]">
-                  {" "}
-                  It was popularised in the 1960s with the release of Letraset
-                  sheets containing Lorem Ipsum passages, and more recently with
-                  desktop publishing software like Aldus PageMaker including
-                  versions of Lorem Ipsum.
-                </div>
-                <div className=" absolute left-[8%] top-1 cursor-pointer">
-                  <img src={Edit} alt="" className="w-[19px] h-[19px]" />
+                <div className="flex flex-col gap-3">
+                  <div className="text-[20px] font-bold">Adebayo</div>
+                  <div className="text-[15px]">Full Name</div>
                 </div>
               </div>
-            </div>
-          </section>
-          <section className="bg-white rounded-md mt-5 py-5 px-2">
-            <h2 className=" text-2xl text-[#262630]">History</h2>
-            <div className="flex justify-between items-center px-5 mt-10">
-              <div className="flex gap-3 items-center">
-                <img
-                  class
-                  src={profile}
-                  alt=""
-                  className="w-[32px] h-[32px] rounded-full object-cover"
-                />
-                <h3 className="text-2xl text-gray-500">My journey in tech</h3>
-              </div>
-              <div className="flex gap-5">
-                <button
-                  className="bg-white border border-1 border-green-300 text-green-300 px-3 py-2 rounded-md hover:opacity-75 transition-all duration-300"
-                  onClick={() => Navigate("/create-blog")}
-                >
-                  Edit post
-                </button>
-                <button
-                  className="bg-white border border-1 border-red-300 text-red-300 px-3 py-2 rounded-md hover:opacity-75 transition-all duration-300"
-                  onClick={() => Navigate("/create-blog")}
-                >
-                  Delete post
-                </button>
-              </div>
-            </div>
-          </section>
-          <section className="bg-white rounded-md mt-5 py-5 px-2">
-            <h2 className=" text-2xl text-[#262630]">Account Settings</h2>
-            <div className="flex flex-col gap-3 px-5 mt-10">
-              <div className="flex gap-3 items-center cursor-pointer">
-                <div className="p-2 bg-gray-300 rounded-full">
-                  <img src={Edited} alt="" className="w-[20px] h-[20px]" />
+              <div className="flex gap-5 mt-6 px-4">
+                <div className="p-4 bg-gray-300 rounded-full">
+                  <img src={Message} alt="" />
                 </div>
-                <div className="text-[18px] font-bold">Change Password</div>
-              </div>
-              <div className="flex gap-3 items-center cursor-pointer">
-                <div className="p-2 bg-gray-300 rounded-full">
-                  <img src={Logout} alt="" className="w-[20px] h-[20px]" />
+                <div className="flex flex-col gap-3">
+                  <div className="text-[20px] font-bold">
+                    adebayoalameen@gmail.com
+                  </div>
+                  <div className="text-[15px]">Email Address</div>
                 </div>
-                <div className="text-[18px] font-bold">Logout</div>
               </div>
-            </div>
-          </section>
-        </div>
-      </main>
-    </section>
+              <div className="flex gap-5 mt-8 px-[40px] relative">
+                <div className="flex flex-col gap-3">
+                  <div className="text-[20px] font-bold">Bio</div>
+                  <div className="text-[15px]">
+                    {" "}
+                    It was popularised in the 1960s with the release of Letraset
+                    sheets containing Lorem Ipsum passages, and more recently
+                    with desktop publishing software like Aldus PageMaker
+                    including versions of Lorem Ipsum.
+                  </div>
+                  <div
+                    className=" absolute left-[8%] top-1 cursor-pointer"
+                    onClick={() => setShowBio(true)}
+                  >
+                    <img src={Edit} alt="" className="w-[19px] h-[19px]" />
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="bg-white rounded-md mt-5 py-5 px-2">
+              <h2 className=" text-2xl text-[#262630]">History</h2>
+              <div className="flex justify-between items-center px-5 mt-10">
+                <div className="flex gap-3 items-center">
+                  <img
+                    class
+                    src={profile}
+                    alt=""
+                    className="w-[32px] h-[32px] rounded-full object-cover"
+                  />
+                  <h3 className="text-2xl text-gray-500">My journey in tech</h3>
+                </div>
+                <div className="flex gap-5">
+                  <button
+                    className="bg-white border border-1 border-green-300 text-green-300 px-3 py-2 rounded-md hover:opacity-75 transition-all duration-300"
+                    onClick={() => Navigate("/create-blog")}
+                  >
+                    Edit post
+                  </button>
+                  <button
+                    className="bg-white border border-1 border-red-300 text-red-300 px-3 py-2 rounded-md hover:opacity-75 transition-all duration-300"
+                    onClick={() => Navigate("/create-blog")}
+                  >
+                    Delete post
+                  </button>
+                </div>
+              </div>
+            </section>
+            <section className="bg-white rounded-md mt-5 py-5 px-2">
+              <h2 className=" text-2xl text-[#262630]">Account Settings</h2>
+              <div className="flex flex-col gap-3 px-5 mt-10">
+                <div className="flex gap-3 items-center cursor-pointer">
+                  <div className="p-2 bg-gray-300 rounded-full">
+                    <img src={Edited} alt="" className="w-[20px] h-[20px]" />
+                  </div>
+                  <div className="text-[18px] font-bold">Change Password</div>
+                </div>
+                <div className="flex gap-3 items-center cursor-pointer">
+                  <div className="p-2 bg-gray-300 rounded-full">
+                    <img src={Logout} alt="" className="w-[20px] h-[20px]" />
+                  </div>
+                  <div className="text-[18px] font-bold">Logout</div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </main>
+      </section>
+    </>
   );
 };
 
