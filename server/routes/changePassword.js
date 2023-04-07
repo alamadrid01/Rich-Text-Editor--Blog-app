e789096740e40a479550acadee514b6e0600a135
Al-ameen <adebayoalameen88@gmail.com>
2023-04-08 04:01:23 +0800

    Fix: Updated password endpoint to accept multipart data form

--- server/routes/changePassword.js
+++ server/routes/changePassword.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const ChangePassword = require("../controllers/profileUpdate/changePassword");
+const multer = require("multer");
 
-router.patch("/change-password/:id", ChangePassword);
+const upload = multer();
+
+router.patch("/change-password/:id", upload.none(), ChangePassword);
 
 module.exports = router;
