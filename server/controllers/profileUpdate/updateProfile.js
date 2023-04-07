a419b11dc588e17993a700db6d95a5b0cc362c95
Al-ameen <adebayoalameen88@gmail.com>
2023-04-07 08:16:28 +0800

    Fix: Modified the update profile component and made the controller accept multi form data and single images

--- server/controllers/profileUpdate/updateProfile.js
+++ server/controllers/profileUpdate/updateProfile.js
@@ -2,44 +2,59 @@ const User = require("../../model/userSchema");
 const mongoose = require("mongoose");
 
 const UpdateProfile = async (req, res) => {
-    const id = req.params.id;
-    const {fName, username, location, bio, available, twitter, instagram, github, stack, facebook, website, link, email} = req.body;
-    // const {image} = req.file;
-    // console.log(image);
+  const id = req.params.id;
+  const {
+    fName,
+    username,
+    location,
+    bio,
+    available,
+    twitter,
+    instagram,
+    github,
+    stack,
+    facebook,
+    website,
+    link,
+    email,
+  } = req.body;
+  const file = req.file;
+  console.log(file);
 
-    if(!id) return res.status(401).json({message: "Id is required"})
+  if (!id) return res.status(401).json({ message: "Id is required" });
 
-        // Check if it is a valid Id
-    if (!mongoose.Types.ObjectId.isValid(id)) {
+  // Check if it is a valid Id
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(401).json({ message: "Invalid ID" });
-    }
+  }
 
-    const links = {
-      facebook,
-      twitter,
-      instagram,
-      stack,
-      github,
-      link,
-      website,
-    };
+  const links = {
+    facebook,
+    twitter,
+    instagram,
+    stack,
+    github,
+    link,
+    website,
+  };
 
-const updatedFields = {
-  bio: bio,
-  fullName: fName,
-  username: username,
-  location: location,
-  available: available,
-  email: email,
-  $set: {
-   socialLinks: [links]
-  },
+  const updatedFields = {
+    bio: bio,
+    fullName: fName,
+    username: username,
+    location: location,
+    available: available,
+    email: email,
+    $set: {
+      socialLinks: [links],
+    },
+  };
+
+  const result = await User.findByIdAndUpdate({ _id: id }, updatedFields, {
+    new: true,
+  }).exec();
+  if (!result) return res.sendStatus(404);
+  res.sendStatus(204);
 };
 
-
-const result = await User.findByIdAndUpdate({_id: id}, updatedFields, {new:true}).exec();
-if (!result) return res.sendStatus(404);
-    res.sendStatus(204);
-}
-
-module.exports =  UpdateProfile
+module.exports = UpdateProfile;
--- server/routes/updateProfile.js
+++ server/routes/updateProfile.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const UpdateProfile = require("../controllers/profileUpdate/updateProfile");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-router.put("/update/:id", UpdateProfile);
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API,
+  api_secret: process.env.CLOUD_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "blog",
+    public_id: (req, file) => Date.now() + "-" + file.originalname,
+  },
+});
+
+const upload = multer({storage: storage});
+
+router.put("/update/:id", upload.single('image'), UpdateProfile);
 
 module.exports = router;
